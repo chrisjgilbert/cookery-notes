3e15d04ca5c697faf89bb8c86a8445e67af26f09
Chris Gilbert <40495757+chrisjgilbert@users.noreply.github.com>
2025-10-22 14:28:56 +0100

    Add some validations

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -6,4 +6,9 @@ class Recipe < ApplicationRecord
   accepts_nested_attributes_for :steps, allow_destroy: true, reject_if: :all_blank
 
   enum :status, {draft: 0, published: 1}
+
+  validates :title, presence: true
+  validates :cook_time_mins, numericality: {greater_than: 0}, allow_nil: true
+  validates :prep_time_mins, numericality: {greater_than: 0}, allow_nil: true
+  validates :servings, numericality: {greater_than: 0}, allow_nil: true
 end
