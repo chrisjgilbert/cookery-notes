e41d81666666b2dce1e9a7302b1c42ee01bb83ff
Chris Gilbert <40495757+chrisjgilbert@users.noreply.github.com>
2025-10-22 10:56:35 +0100

    Add some seeds for dev

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,19 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Recipe.create(
+  title: "Spaghetti Carbonara",
+  description: "A classic Italian dish made with spaghetti, eggs, cheese, and bacon.",
+  source_url: "https://www.allrecipes.com/recipe/23600/worlds-best-spaghetti-carbonara/",
+  source_name: "All Recipes",
+  notes: "A classic Italian dish made with spaghetti, eggs, cheese, and bacon."
+)
+
+Recipe.create(
+  title: "Roast Chicken",
+  description: "A classic dish made with a whole chicken roasted in the oven.",
+  source_url: "https://www.bbcgoodfood.com/recipes/roast-chicken",
+  source_name: "BBC Good Food",
+  notes: "A classic dish made with a whole chicken roasted in the oven with herbs and lemon."
+)
--- test/system/recipes_test.rb
+++ test/system/recipes_test.rb
@@ -6,5 +6,9 @@ class RecipesTest < ApplicationSystemTestCase
 
     assert_selector "h1", text: "Cookery Notes"
     assert_selector "p", text: "Your personal recipe collection"
+    assert_selector "h2", text: Recipe.first.title
+    assert_selector "p", text: Recipe.first.description
+    assert_selector "h2", text: Recipe.last.title
+    assert_selector "p", text: Recipe.last.description
   end
 end
